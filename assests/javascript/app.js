758afacc4b6dd0f870feb4168f00c557f080d688
Kai R <kairichardson2000@gmail.com>
2019-04-20 19:24:24 -0700

    first

---
+++ assests/javascript/app.js
@@ -0,0 +1,7 @@
+$("#my_face").mouseenter(function() {
+    $( this ).attr("src", "./assests/images/my-face-forward-sq.png");
+  });
+
+$("#my_face").mouseleave(function() {
+    $(this).attr("src", "./assests/images/my-face-smile-sq.png");
+  });
