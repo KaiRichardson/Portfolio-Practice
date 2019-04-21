eb3118ff2f292f8f0f935074bf70a1b0d3d22ff0
Kai R <kairichardson2000@gmail.com>
2019-04-20 20:13:07 -0700

    mid 2

--- assests/javascript/app.js
+++ assests/javascript/app.js
@@ -1,7 +1,27 @@
-$("#my_face").mouseenter(function() {
-    $( this ).attr("src", "./assests/images/my-face-forward-sq.png");
-  });
+var prevScrollpos = window.pageYOffset;
 
-$("#my_face").mouseleave(function() {
-    $(this).attr("src", "./assests/images/my-face-smile-sq.png");
-  });
+window.onscroll = function () {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-50px";
+  }
+  prevScrollpos = currentScrollPos;
+}
+
+//animate scroll when a button in clicked
+$(".scrollBtn").on("mouseup", function () {
+  $('html,body').animate({
+    scrollTop: $($(this).attr('href')).offset().top
+  },
+    'slow');
+});
+
+$("#my_face").mouseenter(function () {
+  $(this).attr("src", "./assests/images/my-face-forward-sq.png");
+});
+
+$("#my_face").mouseleave(function () {
+  $(this).attr("src", "./assests/images/my-face-smile-sq.png");
+});
